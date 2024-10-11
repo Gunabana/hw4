4cc0a143ca350a5b046468a2bae40e2ed33494f0
brodybond <149192242+brodybond@users.noreply.github.com>
2024-10-10 21:01:12 -0400

    Fixed to required specifications
    Tristan did not complete the work to specification. I communicated to him the need for changes, but he did not respond via Discord; given it is due tonight, I went ahead and implemented my attempt at task 2.

--- task2.sh
+++ task2.sh
@@ -1,1 +1,3 @@
-for file in file_*; do csc_count=$(grep -o "CSC510" "$file" | wc -l); if grep -q "sample" "$file" && [ "$csc_count" -ge 3 ]; then echo "$csc_count $file"; fi; done | sort -rn | while read count file; do echo "$count $(echo "$file" | sed 's/file_/filtered_/')"; done
+#!/bin/bash
+
+grep -l 'sample' dataset1/file* | xargs -I{} sh -c "count=\$(grep -o 'CSC510' {} | wc -l); size=\$(stat -c%s {}); if [ \$count -ge 3 ]; then echo \$count \$size {}; fi" | sort -k1,1nr -k2,2nr | awk '{print $3}' | sed 's|^dataset1/file_|filtered_|'
